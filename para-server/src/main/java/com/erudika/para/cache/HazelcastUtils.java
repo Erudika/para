633c3ed762f91eec71a884068b2cd06a4ca4c935
Alex Bogdanovski <alex@erudika.com>
2017-08-14 14:54:25 +0300

    fixed Hazelcast AWS plugin and other settings

--- para-server/src/main/java/com/erudika/para/cache/HazelcastUtils.java
+++ para-server/src/main/java/com/erudika/para/cache/HazelcastUtils.java
@@ -21,6 +21,8 @@ import com.erudika.para.DestroyListener;
 import com.erudika.para.Para;
 import com.erudika.para.utils.Config;
 import com.hazelcast.config.AwsConfig;
+import com.hazelcast.config.DiscoveryConfig;
+import com.hazelcast.config.DiscoveryStrategyConfig;
 import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.JoinConfig;
 import com.hazelcast.config.MapConfig;
@@ -31,6 +33,8 @@ import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.config.TcpIpConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Helper functions for {@link HazelcastCache}.
@@ -65,22 +69,38 @@ public final class HazelcastUtils {
 			MapConfig mapcfg = new MapConfig("default");
 			mapcfg.setEvictionPolicy(getEvictionPolicy());
 			mapcfg.setTimeToLiveSeconds(Config.getConfigInt("hc.ttl_seconds", 3600));
+			mapcfg.setMaxIdleSeconds(mapcfg.getTimeToLiveSeconds() * 2);
 	//			mapcfg.setMapStoreConfig(new MapStoreConfig().setEnabled(false).setClassName(NODE_NAME));
 			mapcfg.setMaxSizeConfig(getMaxSize());
 			cfg.addMapConfig(mapcfg);
 			cfg.setProperty("hazelcast.jmx", Boolean.toString(isJMXOn()));
 			cfg.setProperty("hazelcast.logging.type", "slf4j");
-			cfg.setProperty("hazelcast.health.monitoring.level", "OFF");
+			cfg.setProperty("hazelcast.health.monitoring.level", "SILENT");
+
 			if (Config.IN_PRODUCTION && Config.getConfigBoolean("hc.ec2_discovery_enabled", true)) {
-				cfg.setNetworkConfig(new NetworkConfig().setJoin(new JoinConfig().
-					setMulticastConfig(new MulticastConfig().setEnabled(false)).
-						setTcpIpConfig(new TcpIpConfig().setEnabled(false)).
-						setAwsConfig(new AwsConfig().setEnabled(true).
-							setAccessKey(Config.AWS_ACCESSKEY).
-							setSecretKey(Config.AWS_SECRETKEY).
-							setRegion(Config.AWS_REGION).
-							setHostHeader("ec2.amazonaws.com").
-							setSecurityGroupName(Config.getConfigParam("hc.discovery_group", "hazelcast")))));
+				cfg.setProperty("hazelcast.discovery.enabled", "true");
+				cfg.setProperty("hazelcast.discovery.public.ip.enabled", "true");
+
+				Map<String, Comparable> awsConfig = new HashMap<String, Comparable>();
+				awsConfig.put("access-key", Config.AWS_ACCESSKEY);
+				awsConfig.put("secret-key", Config.AWS_SECRETKEY);
+				awsConfig.put("region", Config.AWS_REGION);
+				awsConfig.put("region", Config.AWS_REGION);
+				awsConfig.put("host-header", "ec2.amazonaws.com");
+				awsConfig.put("security-group-name", Config.getConfigParam("hc.discovery_group", "hazelcast"));
+
+				DiscoveryConfig ec2DiscoveryConfig = new DiscoveryConfig();
+				ec2DiscoveryConfig.addDiscoveryStrategyConfig(
+						new DiscoveryStrategyConfig("com.hazelcast.aws.AwsDiscoveryStrategy", awsConfig));
+
+				cfg.setNetworkConfig(new NetworkConfig().
+						setJoin(new JoinConfig().
+							setMulticastConfig(new MulticastConfig().setEnabled(false)).
+							setTcpIpConfig(new TcpIpConfig().setEnabled(false)).
+							setAwsConfig(new AwsConfig().setEnabled(false)).
+							setDiscoveryConfig(ec2DiscoveryConfig)
+						)
+					);
 			}
 
 			hcInstance = Hazelcast.newHazelcastInstance(cfg);
