f8e479e040f5e6e8c72300dc3bc30a476bddded4
Alex Bogdanovski <alex@erudika.com>
2017-12-19 01:27:05 +0200

    added nested invocation checking and reporting when a DAO method calls another DAO method

--- para-server/src/main/java/com/erudika/para/aop/IndexAndCacheAspect.java
+++ para-server/src/main/java/com/erudika/para/aop/IndexAndCacheAspect.java
@@ -26,6 +26,7 @@ import com.erudika.para.core.ParaObject;
 import com.erudika.para.persistence.DAO;
 import com.erudika.para.search.Search;
 import com.erudika.para.utils.Config;
+import com.erudika.para.utils.Utils;
 import com.erudika.para.validation.ValidationUtils;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -104,6 +105,7 @@ public class IndexAndCacheAspect implements MethodInterceptor {
 		Cached cachedAnno = null;
 
 		try {
+			detectNestedInvocations(m);
 			superMethod = DAO.class.getMethod(m.getName(), m.getParameterTypes());
 			indexedAnno = Config.isSearchEnabled() ? superMethod.getAnnotation(Indexed.class) : null;
 			cachedAnno = Config.isCacheEnabled() ? superMethod.getAnnotation(Cached.class) : null;
@@ -341,4 +343,23 @@ public class IndexAndCacheAspect implements MethodInterceptor {
 		}
 	}
 
+	/**
+	 * Try and detect if a DAO method is called from another public DAO method, annotated with {@link Indexed} or
+	 * {@link Cached}. It causes that method to be intercepted twice and objects will be indexed/cached twice.
+	 * @param mi method invocation
+	 */
+	private void detectNestedInvocations(Method daoMethod) {
+		StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+		for (StackTraceElement stackTraceElement : stackTraceElements) {
+			if (daoMethod.getDeclaringClass().getName().equals(stackTraceElement.getClassName()) &&
+					!daoMethod.getName().equals(stackTraceElement.getMethodName())) {
+				String error = Utils.formatMessage("Method {0}.{1}() invoked from another method in the same "
+						+ "class - {2}.{3}(). DAO implementations should avoid this as it causes objects to be "
+						+ "indexed and cached twice per request.",
+						daoMethod.getDeclaringClass().getSimpleName(), daoMethod.getName(),
+						stackTraceElement.getClassName(), stackTraceElement.getMethodName());
+				throw new RuntimeException(error);
+			}
+		}
+	}
 }
