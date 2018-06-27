e7e3465e111872781bb012d4e2f2f28f94b6610d
Pavel <bitnot@users.noreply.github.com>
2018-06-27 10:04:17 +0700

    Convert email to lower case for Gravatar hashing
    Gravatar stores emails in lower case while para does not.

--- para-core/src/main/java/com/erudika/para/core/User.java
+++ para-core/src/main/java/com/erudika/para/core/User.java
@@ -729,7 +729,7 @@ public class User implements ParaObject {
 	private void setGravatarPicture() {
 		if (StringUtils.isBlank(picture)) {
 			if (email != null) {
-				String emailHash = Utils.md5(email);
+				String emailHash = Utils.md5(email.toLowerCase());
 				setPicture("https://www.gravatar.com/avatar/" + emailHash + "?size=400&d=mm&r=pg");
 			} else {
 				setPicture("https://www.gravatar.com/avatar?d=mm&size=400");
