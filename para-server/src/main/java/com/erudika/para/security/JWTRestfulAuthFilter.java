e1b64a6ec5ade5e870327e24e58cc4b7a8f1b9be
Alex Bogdanovski <alex@erudika.com>
2019-06-13 11:34:25 +0300

    changed logging level to debug for invalid delegated tokens

--- para-server/src/main/java/com/erudika/para/security/JWTRestfulAuthFilter.java
+++ para-server/src/main/java/com/erudika/para/security/JWTRestfulAuthFilter.java
@@ -475,7 +475,7 @@ public class JWTRestfulAuthFilter extends GenericFilterBean {
 			// - if the generic OAuth 2 filter is used
 			if ("oauth2".equalsIgnoreCase(identityProvider) && oauth2Auth.isAccessTokenDelegationEnabled(app) &&
 					!oauth2Auth.isValidAccessToken(app, user.getPassword())) {
-				logger.info("The access token delegated from '" + identityProvider + "' is invalid for " +
+				logger.debug("The access token delegated from '" + identityProvider + "' is invalid for " +
 						user.getAppid() + "/" + user.getId());
 				throw new AuthenticationServiceException("The access token delegated from '" +
 						identityProvider + "' is invalid.");
