e70b583a944e68c2cbec78cad4882a4cc3d748d7
Alex Bogdanovski <alex@erudika.com>
2019-08-06 01:24:57 +0300

    reverted option for jwt=incookie

--- para-server/src/main/java/com/erudika/para/security/SimpleAuthenticationSuccessHandler.java
+++ para-server/src/main/java/com/erudika/para/security/SimpleAuthenticationSuccessHandler.java
@@ -65,10 +65,6 @@ public class SimpleAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 				if (StringUtils.contains(customURI, "jwt=?")) {
 					SignedJWT newJWT = SecurityUtils.generateJWToken(u, app);
 					customURI = customURI.replace("jwt=?", "jwt=" + newJWT.serialize());
-				} else if (StringUtils.contains(customURI, "jwt=incookie")) {
-					SignedJWT newJWT = SecurityUtils.generateJWToken(u, app);
-					HttpUtils.setStateParam(app.getAppIdentifier().trim() + "-auth",
-							newJWT.serialize(), request, response, true);
 				}
 				if (!StringUtils.isBlank(customURI)) {
 					redirectStrategy.sendRedirect(request, response, customURI);
