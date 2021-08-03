ba0ff8c91693d0fc9cf2ace97450da88465e28ca
Alex Bogdanovski <alex@erudika.com>
2021-08-03 23:28:57 +0300

    added more verbose loggin on errors in the GenericOAuth2Filter

--- para-server/src/main/java/com/erudika/para/security/filters/GenericOAuth2Filter.java
+++ para-server/src/main/java/com/erudika/para/security/filters/GenericOAuth2Filter.java
@@ -312,12 +312,18 @@ public class GenericOAuth2Filter extends AbstractAuthenticationProcessingFilter 
 
 		try (CloseableHttpResponse resp2 = httpclient.execute(profileGet)) {
 			HttpEntity respEntity = resp2.getEntity();
-			if (respEntity != null && resp2.getStatusLine().getStatusCode() == HttpServletResponse.SC_OK) {
-				profile = jreader.readValue(respEntity.getContent());
+			Map<String, Object> error = null;
+			if (respEntity != null) {
+				if (resp2.getStatusLine().getStatusCode() == HttpServletResponse.SC_OK) {
+					profile = jreader.readValue(respEntity.getContent());
+				} else {
+					error = jreader.readValue(respEntity.getContent());
+				}
 			}
-			if (profile == null || profile.isEmpty()) {
-				LOG.error("OAuth 2 provider did not return any valid user information - response code {} {}, app '{}'",
-						resp2.getStatusLine().getStatusCode(), resp2.getStatusLine().getReasonPhrase(), app.getId());
+			if (profile == null || profile.isEmpty() || error != null) {
+				LOG.error("OAuth 2 provider did not return any valid user information - "
+						+ "response code {} {}, app '{}', payload {}",
+						resp2.getStatusLine().getStatusCode(), resp2.getStatusLine().getReasonPhrase(), app.getId(), error);
 			}
 			EntityUtils.consumeQuietly(respEntity);
 		}
