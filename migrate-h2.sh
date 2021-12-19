f17691b2173aba0611ee1b349c2c4ca05306e57a
Alex Bogdanovski <alex@erudika.com>
2021-12-19 17:17:47 +0200

    added H2 migration script

---
+++ migrate-h2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+OLD_H2_VERSION="1.4.200"
+NEW_H2_VERSION="2.0.202"
+PARA_USER="para"
+PARA_PASS="secret"
+
+wget -q -O h2-$NEW_H2_VERSION.jar https://search.maven.org/remotecontent?filepath=com/h2database/h2/2.0.202/h2-2.0.202.jar && \
+wget -q -O h2-$OLD_H2_VERSION.jar https://search.maven.org/remotecontent?filepath=com/h2database/h2/1.4.200/h2-1.4.200.jar && \
+
+java -cp h2-${OLD_H2_VERSION}.jar org.h2.tools.Script -url jdbc:h2:./para -user $PARA_USER -password $PARA_PASS -script para.sql && \
+rm para.mv.db para.trace.db && \
+java -cp h2-${NEW_H2_VERSION}.jar org.h2.tools.RunScript -url jdbc:h2:./para -user $PARA_USER -password $PARA_PASS -script para.sql && \
+rm h2-$NEW_H2_VERSION.jar h2-$OLD_H2_VERSION.jar && \
+
+echo "Migrated Para DB from $OLD_H2_VERSION to $NEW_H2_VERSION." || \
+
+echo "Migration failed."
