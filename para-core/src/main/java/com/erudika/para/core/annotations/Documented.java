96c70ecd921a9dd17f2b5416836ec20017527cec
Alex Bogdanovski <alex@erudika.com>
2022-02-10 20:10:03 +0200

    added @Documented annotation

---
+++ para-core/src/main/java/com/erudika/para/core/annotations/Documented.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2013-2022 Erudika. http://erudika.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For issues and patches go to: https://github.com/erudika
+ */
+package com.erudika.para.core.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A generic annotation intended to be used for attaching documentation
+ * metadata to the getter methods of configuration properties.
+ * @author Alex Bogdanovski [alex@erudika.com]
+ */
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Documented {
+
+	/**
+	 * @return the category which this property is part of.
+	 */
+	String category() default "main";
+
+	/**
+	 * @return basic description of the config property.
+	 */
+	String description() default "";
+
+	/**
+	 * @return the data type of the config value.
+	 */
+	String type() default "string";
+
+	/**
+	 * @return a default or suggested value.
+	 */
+	String value() default "";
+
+	/**
+	 * @return the config property key (identifier).
+	 */
+	String identifier() default "";
+
+}
