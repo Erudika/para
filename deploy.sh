1f43372e7101d34eb9ca560d597b4eec54a6d5cb
Alex Bogdanovski <alex@erudika.com>
2022-02-11 16:52:14 +0200

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,12 @@ git add -A && git commit -m "Release v$ver." && git push origin master
 mvn --batch-mode -Dtag=v${ver} release:prepare -Dresume=false -DreleaseVersion=${ver} -DdevelopmentVersion=${devver}-SNAPSHOT && \
 mvn release:perform && \
 echo "Maven release done, publishing release on GitHub..," && \
-echo "v$ver" > changelog.txt && \
-echo "" >> changelog.txt && \
 git log $lastver..HEAD --oneline >> changelog.txt && \
 echo "" >> changelog.txt && \
 echo "### :package: [Download JAR](https://oss.sonatype.org/service/local/repositories/releases/content/com/erudika/para-jar/${ver}/para-jar-${ver}.jar)" >> changelog.txt && \
 echo "" >> changelog.txt && \
 echo "### :package: [Download WAR](https://oss.sonatype.org/service/local/repositories/releases/content/com/erudika/para-war/${ver}/para-war-${ver}.war)" >> changelog.txt && \
-hub release create -F changelog.txt "v$ver" && \
+hub release create -F changelog.txt -t "v$ver" "v$ver" && \
 rm changelog.txt
 
 
