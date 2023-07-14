d894cc28573ac2ce05ff27486470b51993e3bbfa
Alex Bogdanovski <alex@erudika.com>
2023-07-14 15:27:57 +0300

    updated H2 migration script

--- migrate-h2.sh
+++ migrate-h2.sh
@@ -2,13 +2,13 @@
 
 # Run this script from inside the Para ./data folder, or wherever para.mv.db is located
 
-OLD_H2_VERSION="1.4.200"
-NEW_H2_VERSION="2.0.202"
+OLD_H2_VERSION="2.1.214"
+NEW_H2_VERSION="2.2.220"
 PARA_USER="para"
 PARA_PASS="secret"
 
-wget -q -O h2-$NEW_H2_VERSION.jar https://search.maven.org/remotecontent?filepath=com/h2database/h2/2.0.202/h2-2.0.202.jar && \
-wget -q -O h2-$OLD_H2_VERSION.jar https://search.maven.org/remotecontent?filepath=com/h2database/h2/1.4.200/h2-1.4.200.jar && \
+wget -q -O h2-$NEW_H2_VERSION.jar https://search.maven.org/remotecontent?filepath=com/h2database/h2/$NEW_H2_VERSION/h2-$NEW_H2_VERSION.jar && \
+wget -q -O h2-$OLD_H2_VERSION.jar https://search.maven.org/remotecontent?filepath=com/h2database/h2/$OLD_H2_VERSION/h2-$OLD_H2_VERSION.jar && \
 
 java -cp h2-${OLD_H2_VERSION}.jar org.h2.tools.Script -url jdbc:h2:./para -user $PARA_USER -password $PARA_PASS -script para.sql && \
 rm para.mv.db para.trace.db && \
